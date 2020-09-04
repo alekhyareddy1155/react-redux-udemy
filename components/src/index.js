c3b1166c187ec3d022e8c14bb33ca4b8b35b707a
Reddy <Reddy.Alekhya@go-mmt.com>
2020-09-04 15:25:27 +0530

    added approvalcard basic component

--- components/src/index.js
+++ components/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 const App=()=>{
    return (
     <div className="ui container comments">
@@ -9,6 +11,7 @@ const App=()=>{
           <h4>Warning!</h4>
           Are you sure you want to do this?
         </div>
+        <ApprovalCard />
         <CommentDetail
           author="Sam"
           timeAgo="Today at 4:45PM"
