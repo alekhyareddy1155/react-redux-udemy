b74009f6e101759110b393defe3ef63d798e1fbc
Reddy <Reddy.Alekhya@go-mmt.com>
2020-09-04 15:11:29 +0530

    added components

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 const App=()=>{
    return (
-    <div>
-      hi!
+    <div className="ui container comments">
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+          avatar={faker.image.avatar()}
+        />
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="I like the writing"
+          avatar={faker.image.avatar()}
+        />
     </div>
-  )
-}
+    );
+};
 
 ReactDOM.render(<App />,
     document.getElementById("root"));
